7ad0d30d4ef03b52d139ff538eb4aaec3ce69bb2
Moh <mkashani.phd@gmail.com>
2025-10-16 08:27:39 -0700

    testing the rtio extention

--- Experiments/Experiments/flake.nix
+++ Experiments/Experiments/flake.nix
@@ -1,13 +1,8 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-
-    # Upstream ARTIQ-Zynq (release-7)
     artiq-zynq.url = "git+https://git.m-labs.hk/m-labs/artiq-zynq?ref=release-7";
-    artiq-zynq.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Your local myrtioext repo (adjust path)
-    myrtioext.url = "path:/home/jrydberg/Documents/Projects/Artiq_envs/defult/Artiq_VM/Experiments/myrtioext";
+    myrtioext.url = "path:./Experiments/myrtioext";
   };
 
   outputs = { self, nixpkgs, artiq-zynq, myrtioext }:
@@ -15,7 +10,6 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
 
-    # Build your Python package (just the derivation; we’re not forcing it into upstream’s env yet)
     myrtioextPkg = pkgs.python3Packages.buildPythonPackage {
       pname = "myrtioext";
       version = "0.1.0";
@@ -25,20 +19,17 @@
       propagatedBuildInputs = [ ];
     };
 
-    # Choose target/variant/json (adjust to your JSON path)
     target   = "kasli_soc";
     variant  = "standalone";
+    # TODO: change this JSON path to your actual kasli-soc JSON
     jsonPath = /home/jrydberg/Documents/Projects/Artiq_envs/defult/kasli-soc-standalone_node1_with_edgecounters_en.json;
 
-    # Call upstream helper and safely select the SD image attr (hyphenated)
     pkgSet  = artiq-zynq.makeArtiqZynqPackage { inherit target variant; json = jsonPath; };
     sdName  = "${target}-${variant}-sd";
     sdImage = builtins.getAttr sdName pkgSet;
   in {
-    # Build with: nix build .#sd --print-build-logs --impure
     packages.${system}.sd = sdImage;
 
-    # (Optional) dev shell to sanity-check your myrtioext package builds & imports
     devShells.${system}.default = pkgs.mkShell {
       packages = [ pkgs.python3 myrtioextPkg ];
       shellHook = ''
